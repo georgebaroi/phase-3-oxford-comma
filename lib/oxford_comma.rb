7b36d9efd6ac7ce786ed4c918d6d26cabf889b73
George Baroi <baroi.george@gmail.com>
2022-09-04 03:42:16 -0400

    done with assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
+#takes an array of string elements as an argument 
+#and converts it into a string using the Oxford comma
+#oxford_comma(["fiddleheads", "okra", "kohlrabi"])
 def oxford_comma(array)
+   return array.join(" and ") if array.size < 3
+   array[-1] = "and #{array[-1]}"
+   array.join(", ")
+
 
 end
